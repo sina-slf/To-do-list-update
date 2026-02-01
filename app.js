d73282c7a664e5e5345d29e6ddb0b2ceffe3e403
sina-slf <sina.firozy.1387@gmail.com>
2026-02-01 17:09:36 +0330

    Add task management and dark mode functionality

---
+++ app.js
@@ -0,0 +1,85 @@
+const input = document.getElementById("taskinput");
+const list = document.getElementById("list");
+const btn = document.getElementById("addBtn");
+const trashSrc = "trash.png";
+
+btn.addEventListener("click", addtask)
+function addtask() {
+    if(input.value.trim() === "")return;
+
+const li = document.createElement("li");
+li.textContent = input.value + " ";
+
+const deleatimg = document.createElement("img");
+deleatimg.src = trashSrc;
+
+deleatimg.onclick = () => li.remove();
+
+li.appendChild(deleatimg);
+list.appendChild(li);
+input.value = "";
+};
+
+const DarkMood = document.getElementById("DarkMood");
+ 
+DarkMood.addEventListener("click", (m) => {
+    m.stopPropagation();
+    DarkMood.classList.toggle("active");
+    document.body.classList.toggle("dark")
+
+});
+
+const hamb = document.getElementById("Hamb");
+const Menu = document.getElementById("Menu");
+
+hamb.addEventListener("click", () => {
+    Menu.classList.toggle("open");
+});
+const input = document.getElementById("taskinput");
+const list = document.getElementById("list");
+const btn = document.getElementById("addBtn");
+const trashSrc = "trash.png";
+
+btn.addEventListener("click", addtask)
+function addtask() {
+    if(input.value.trim() === "")return;
+
+const li = document.createElement("li");
+li.textContent = input.value + " ";
+
+const deleatimg = document.createElement("img");
+deleatimg.src = trashSrc;
+
+deleatimg.onclick = () => li.remove();
+
+li.appendChild(deleatimg);
+list.appendChild(li);
+input.value = "";
+};
+
+const DarkMood = document.getElementById("DarkMood");
+ 
+DarkMood.addEventListener("click", (e) => {
+    e.stopPropagation();
+    DarkMood.classList.toggle("active");
+    document.body.classList.toggle("dark");
+
+});
+
+const hamb = document.getElementById("Hamb");
+const Menu = document.getElementById("Menu");
+
+hamb.addEventListener("click", () => {
+    Menu.classList.toggle("open");
+    document.body.classList.toggle("blur");
+});
+
+document.addEventListener("click", (e) => {
+
+    const clickmenu = Menu.contains(e.target);
+    const clickhamb = hamb.contains(e.target);
+
+    if(!clickhamb && !clickmenu) {
+        Menu.classList.remove("open");
+    }
+});
